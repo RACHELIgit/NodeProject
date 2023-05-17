4e2c5586dc916a5d6444d4fc8fd52ce25a7574d8
leah <leah.fullstack@gmail.com>
2023-05-17 18:13:40 +0300

    validation mail

--- validation.js
+++ validation.js
@@ -1,25 +1,25 @@
 const axios = require("axios");
 
 module.exports = {
-  Email: function (email) {
-    const apiKey = "f17a068c505541138562bfc860be30c7";
-    const apiUrl = `https://emailvalidation.abstractapi.com/v1/?api_key==${apiKey}&email=${email}`;
-    //
+  // Email: function (email) {
+  //   const apiKey = "f17a068c505541138562bfc860be30c7";
+  //   const apiUrl = `https://emailvalidation.abstractapi.com/v1/?api_key==${apiKey}&email=${email}`;
+  //   //
 
-    axios
-      .get(apiUrl)
-      .then((response) => {
-        const isValid = response.data.is_valid;
-        if (isValid) {
-          console.log(`${email} is a valid email address.`);
-        } else {
-          console.log(`${email} is an invalid email address.`);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  },
+  //   axios
+  //     .get(apiUrl)
+  //     .then((response) => {
+  //       const isValid = response.data.is_valid;
+  //       if (isValid) {
+  //         console.log(`${email} is a valid email address.`);
+  //       } else {
+  //         console.log(`${email} is an invalid email address.`);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // },
   phone: async function (req, res, next) {
     axios
       .get(
@@ -34,16 +34,14 @@ module.exports = {
         console.log(error);
       });
   },
-  phone1: (req, res) => {
-    axios
-      .get(
-        `https://phonevalidation.abstractapi.com/v1/?api_key=e8c51abf2edf43f2ab415fada836721a&phone=${phone}`
-      )
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-};
+  email: async function (req, res, next) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+      {
+        req.validMail = true;
+        next();     
+       }
+        else
+        console.log("not valid email");
+  
+}
+}
