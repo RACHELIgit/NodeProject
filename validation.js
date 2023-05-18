6c89d46f004507a40d87e5d1760aa24ba3fc9888
RACHELIgit <128489099+RACHELIgit@users.noreply.github.com>
2023-05-18 10:02:17 +0300

    try

--- validation.js
+++ validation.js
@@ -1,25 +1,7 @@
 const axios = require("axios");
 
 module.exports = {
-  // Email: function (email) {
-  //   const apiKey = "f17a068c505541138562bfc860be30c7";
-  //   const apiUrl = `https://emailvalidation.abstractapi.com/v1/?api_key==${apiKey}&email=${email}`;
-  //   //
 
-  //   axios
-  //     .get(apiUrl)
-  //     .then((response) => {
-  //       const isValid = response.data.is_valid;
-  //       if (isValid) {
-  //         console.log(`${email} is a valid email address.`);
-  //       } else {
-  //         console.log(`${email} is an invalid email address.`);
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // },
   phone: async function (req, res, next) {
  
     const uaPhoneNumber = `972${req.body?.phone?.slice(1)}`;
